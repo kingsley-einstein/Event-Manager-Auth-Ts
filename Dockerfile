d62c1272af6512fb07c63150c8ab0c96f8c3bb8a
Kingsley Victor <javaprodigy56@gmail.com>
2020-01-09 23:08:16 +0100

    Add Dockerfile and update config

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:10-alpine
+COPY src ./src
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build:unix
+ENTRYPOINT ["npm", "run", "start:kinto"]
--- src/config/index.ts
+++ src/config/index.ts
@@ -11,6 +11,15 @@ export default (app: any) => {
     app.use(Cors("*"));
     app.use(logger("dev"));
     app.use(actuator());
+
+    //Provide health check endpoint for Kintohub
+    app.get("/", (req: any, res: any) => {
+      res.status(200).json({
+        status: "UP"
+      });
+    });
+
+    // API routes
     app.use("/api/v1", router);
   };
 };
